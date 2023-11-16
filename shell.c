9691b48dc192941b2543fad866035ec17e8797d7
tasneem04 <eng.wr.tasneem@gmail.com>
2023-11-16 13:49:11 -0800

    Fix 1

--- shell.c
+++ shell.c
@@ -1,31 +1,42 @@
 #include "shell.h"
 /** main :the main function to show the prompt */
 
-int main(void) {
+int main(void)
+{
     char *args[2];
     char command[100];
-pid_t pid ;
-    while(1) {
+    pid_t pid ;
+    
+    while(1) 
+    {
         printf("#cisfun$ ");
         fgets(command, sizeof(command), stdin);
         command[strcspn(command, "\n")] = 0;
+ 	if (strcmp(command, "exit") == 0)
+	{
+            printf("Goodbye!\n");
+            break;
+        }
 
         args[0] = command;
         args[1] = NULL;
 
          pid = fork();
-        if (pid == -1) {
+        if (pid == -1) 
+	{
             perror("fork");
             exit(EXIT_FAILURE);
         }
 
-        if (pid == 0) {
+        if (pid == 0)
+       	{
            if (execve(args[0], args, NULL) == -1) 
 	   {
                 perror("execve");
                 exit(EXIT_FAILURE);
             }
-        } else {
+        } else 
+	{
            
             int status;
             waitpid(pid, &status, 0);
